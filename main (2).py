f25b0836476430cb7c2e65ddedc49c767489d54c
deepika-1722 <langanoorunavadeepika@gmail.com>
2025-12-24 10:54:33 +0530

    Update main (2).py

--- main (2).py
+++ main (2).py
@@ -1,4 +1,13 @@
 x=int(input())
 for i in range(1,x+1):
         print("*"*i)
+
+
+
+second method
+x=int(input())
+for i in range(x):
+    for j in range(i+1):
+        print("*",end=" ")
+    print()
     
